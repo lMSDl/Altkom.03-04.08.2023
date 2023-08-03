3598619de42d45757543943d2ec893653da8b8e8
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-03 10:42:38 +0200

    Projekt startowy

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+//Prosimy konsolę, o wypisanie tekstu poprzez wywołanie funkcji Write (funkcje mają zawsze nawiasy)
+Console.Write("Hello, World!");
+//funkcja WriteLine stawia automatycznie znak nowej linii na końcu wskazanego tekstu
+Console.WriteLine("Hello, World!");
+// \n - ręcznie wstawiamy znak nowej linii
+// \ (ukośnik opadający) - aktywacja znaku specjalnego, który następuje po nim
+Console.Write("Hello, World!\n");
+Console.Write("Hello, World!");
