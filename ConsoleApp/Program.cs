e564d98f4559e0bb231c30607c210217ee22dd56
Paweł Łęczycki <pawel@msd.info.pl>
2023-08-03 11:14:22 +0200

    deklaracja, inicjalizacja i przypisanie wartości

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿
+//deklaracja zmiennej lokalej o typie string (ciąg znaków) i nazwie helloWorldValue (nazwa dowolna)
+// ; - oznacza koniec instrukcji
+string helloWorldValue;
+//inicjalizacja zmiennej - pierwsze przypisanie wartości
+helloWorldValue = "Hello, W0rld!";
+
+//deklaracja z inicjalizacją
+string helloWorldWithNewLine = "Hello, World!\n";
+
 
 //Prosimy konsolę, o wypisanie tekstu poprzez wywołanie funkcji Write (funkcje mają zawsze nawiasy)
-Console.Write("Hello, World!");
+Console.Write(helloWorldValue);
 //funkcja WriteLine stawia automatycznie znak nowej linii na końcu wskazanego tekstu
-Console.WriteLine("Hello, World!");
+Console.WriteLine(helloWorldValue);
 // \n - ręcznie wstawiamy znak nowej linii
 // \ (ukośnik opadający) - aktywacja znaku specjalnego, który następuje po nim
-Console.Write("Hello, World!\n");
-Console.Write("Hello, World!");
+Console.Write(helloWorldWithNewLine);
+
+//przypisanie wartości
+helloWorldValue = "Hello, world!";
+
+Console.Write(helloWorldValue);
+
+
